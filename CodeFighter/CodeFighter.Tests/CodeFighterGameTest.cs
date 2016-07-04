9f1bbf84443aeba8ccb5f78dc019672f882cccbf
ernestoangeles <ernesto.angeles@gmail.com>
2016-07-04 11:54:44 -0500

    adding controller folder

--- CodeFighter/CodeFighter.Tests/CodeFighterGameTest.cs
+++ CodeFighter/CodeFighter.Tests/CodeFighterGameTest.cs
@@ -14,6 +14,37 @@ namespace CodeFighter.Tests
             game = new CodeFighterGame();
         }
 
+        //[TestMethod]
+        //public void Jugador2Con10VidaMenosDespuesDePuñoJugador1()
+        //{
+        //    //arrange
+        //    //act
+        //    game.GolpePuñoJugador1();
+        //    var vida2 = game.VidaJugador2;
+        //    //assert
+        //    Assert.AreEqual("190", vida2);
+        //}
 
+        //[TestMethod]
+        //public void Jugador1Con10VidaMenosDespuesDePuñoJugador2()
+        //{
+        //    //arrange
+        //    //act
+        //    game.GolpePuñoJugador2();
+        //    var vida1 = game.VidaJugador1;
+        //    //assert
+        //    Assert.AreEqual("190", vida1);
+        //}
+        
+        //[TestMethod]
+        //public void Jugador1Con5sDespuesDePuñoJugador2()
+        //{
+        //    //arrange
+        //    //act
+        //    game.GolpePuñoJugador2();
+        //    var vida1 = game.VidaJugador1;
+        //    //assert
+        //    Assert.AreEqual("190", vida1);
+        //}
     }
 }
