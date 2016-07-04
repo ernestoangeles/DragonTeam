3805670b136c38fea0214f531dcb252587716d23
ernestoangeles <ernesto.angeles@gmail.com>
2016-07-04 12:08:56 -0500

    escenario 2 completo

--- CodeFighter/CodeFighter.Tests/CodeFighterGameTest.cs
+++ CodeFighter/CodeFighter.Tests/CodeFighterGameTest.cs
@@ -14,37 +14,48 @@ namespace CodeFighter.Tests
             game = new CodeFighterGame();
         }
 
-        //[TestMethod]
-        //public void Jugador2Con10VidaMenosDespuesDePuñoJugador1()
-        //{
-        //    //arrange
-        //    //act
-        //    game.GolpePuñoJugador1();
-        //    var vida2 = game.VidaJugador2;
-        //    //assert
-        //    Assert.AreEqual("190", vida2);
-        //}
+        [TestMethod]
+        public void Jugador2Con10VidaMenosDespuesDePuñoJugador1()
+        {
+            //arrange
+            //act
+            game.GolpePuñoJugador1();
+            var vida2 = game.VidaJugador2;
+            //assert
+            Assert.AreEqual(190, vida2);
+        }
 
-        //[TestMethod]
-        //public void Jugador1Con10VidaMenosDespuesDePuñoJugador2()
-        //{
-        //    //arrange
-        //    //act
-        //    game.GolpePuñoJugador2();
-        //    var vida1 = game.VidaJugador1;
-        //    //assert
-        //    Assert.AreEqual("190", vida1);
-        //}
+        [TestMethod]
+        public void Jugador1Con10VidaMenosDespuesDePuñoJugador2()
+        {
+            //arrange
+            //act
+            game.GolpePuñoJugador2();
+            var vida1 = game.VidaJugador1;
+            //assert
+            Assert.AreEqual(190, vida1);
+        }
         
-        //[TestMethod]
-        //public void Jugador1Con5sDespuesDePuñoJugador2()
-        //{
-        //    //arrange
-        //    //act
-        //    game.GolpePuñoJugador2();
-        //    var vida1 = game.VidaJugador1;
-        //    //assert
-        //    Assert.AreEqual("190", vida1);
-        //}
+        [TestMethod]
+        public void Jugador1Gana5DeEnergiaDespuesDeGolpePuño()
+        {
+            //arrange
+            //act
+            game.GolpePuñoJugador1();
+            var energia1 = game.EnergiaJugador1;
+            //assert
+            Assert.AreEqual(5, energia1);
+        }
+
+        [TestMethod]
+        public void Jugador2Gana5DeEnergiaDespuesDeGolpePuño()
+        {
+            //arrange
+            //act
+            game.GolpePuñoJugador2();
+            var energia1 = game.EnergiaJugador2;
+            //assert
+            Assert.AreEqual(5, energia1);
+        }
     }
 }
--- CodeFighter/CodeFighter/Models/CodefighterGame.cs
+++ CodeFighter/CodeFighter/Models/CodefighterGame.cs
@@ -7,5 +7,29 @@ namespace CodeFighter.Models
 {
     public class CodeFighterGame
     {
+        public int VidaJugador1;
+        public int VidaJugador2;
+        public int EnergiaJugador1;
+        public int EnergiaJugador2;
+
+        public CodeFighterGame()
+        {
+            VidaJugador1 = 200;
+            VidaJugador2 = 200;
+            EnergiaJugador1 = 0;
+            EnergiaJugador2 = 0;
+        }
+
+        public void GolpePuñoJugador1()
+        {
+            VidaJugador2 = VidaJugador2 - 10;
+            EnergiaJugador1 = EnergiaJugador1 + 5;
+        }
+
+        public void GolpePuñoJugador2()
+        {
+            VidaJugador1 = VidaJugador1 - 10;
+            EnergiaJugador2 = EnergiaJugador2 + 5;
+        }
     }
 }
